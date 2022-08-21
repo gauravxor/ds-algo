b89ae335b191a635e0fb146364eb71d9c13d8105
timecop <gauravxor@gmail.com>
2022-08-22 02:25:00 +0530

    Added more coded

---
+++ tuf_a2z_sheet/S1_LEARN_THE_BASICS/S1_3_KNOW_BASIC_MATHS/reverse_a_number.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+
+int reverse_number(int number)
+{
+	int reversed_number = 0;
+	while(number != 0)
+	{
+		int digit = number % 10;
+		reversed_number = reversed_number * 10 + digit;
+		number /= 10;
+	}
+	return reversed_number;
+}
+
+
+
+int main()
+{
+	cout << "Enter the number\n";
+	int number;
+	cin >> number;
+
+	int reversed_number = reverse_number(number);
+	cout << "The reversed number is " << reversed_number << "\n";
+	return 0;
+
+}
