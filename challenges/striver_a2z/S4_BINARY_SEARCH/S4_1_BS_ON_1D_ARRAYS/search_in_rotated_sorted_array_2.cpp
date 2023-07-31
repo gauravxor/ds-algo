7802c26465069f5c3f0bc0f5b5d32cebb21d7791
gauravxor <gauravxor@gmail.com>
2023-08-01 03:10:24 +0530

    Code to search an element in a rotated sorted array, containing duplicates

--- challenges/striver_a2z/S4_BINARY_SEARCH/S4_1_BS_ON_1D_ARRAYS/search_in_rotated_sorted_array_2.cpp
+++ challenges/striver_a2z/S4_BINARY_SEARCH/S4_1_BS_ON_1D_ARRAYS/search_in_rotated_sorted_array_2.cpp
@@ -1,14 +1,27 @@
+/*  Given a rotated sorted array and an element X. Find if X is present in the array
+
+    Hint :  Check for sorted segments and perform the search there if the X lies there,
+            or else perform the search in other unsorted sergment.
+*/
+
 #include <iostream>
 #include <vector>
 using namespace std;
 
 int search(vector<int> &v, int target) {
-    int low = 0, high = v.size() - 1;
+    int low = 0;
+    int high = v.size() - 1;
+
     while(low <= high) {
         int mid = (low + high) / 2;
-        if(target == v[mid])
-            return true;
 
+        if(target == v[mid])
+            return mid;
+
+        /*
+            If all three elements becomes equal then we cannot determine which half is sorted
+            So we move the pointers accordingly till the three elements become different.
+        */
         if(v[low] == v[mid] && v[mid] == v[high]) {
             low++;
             high--;
@@ -22,13 +35,13 @@ int search(vector<int> &v, int target) {
                 low = mid + 1;
         }
         else {
-            if(target >= v[mid + 1] && target <= v[high])
+            if(target >= v[mid] && target <= v[high])
                 low = mid;
             else
                 high = mid - 1;
         }
     }
-    return false;
+    return -1;
 }
 
 int main() {
@@ -41,7 +54,8 @@ int main() {
     int target;
     cin >> target;
 
-    int isPresent = search(v, target);
-    (isPresent) ? cout << "Found\n" : cout << "Not Found\n";
+    int index = search(v, target);
+    cout << index << "\n";
+
     return 0;
 }
