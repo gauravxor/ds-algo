21af5f276802075aa03aea3af57f3c739b10d919
gauravxor <gauravxor@gmail.com>
2023-08-01 00:10:11 +0530

    Code to find the lower bound

---
+++ challenges/striver_a2z/S4_BINARY_SEARCH/S4_1_BS_ON_1D_ARRAYS/lower_bound.cpp
@@ -0,0 +1,41 @@
+/* Y is lower bound of X, means Y is a number such that [Y >= X] */
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int lower_bound(vector<int> &v, int target) {
+
+    /* If all elements are smaller than the target*/
+    if(v[v.size() - 1] < target)
+        return -1;
+
+    int low = 0;
+    int high = v.size() - 1;
+
+    while(low < high) {
+        int mid = (low + high) / 2;
+
+        if(v[mid] < target) {
+            low = mid + 1;
+        }
+        else {
+            high = mid;
+        }
+    }
+    return high;
+}
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> v(n);
+    for(auto &it : v)
+        cin >> it;
+
+    int key;
+    cin >> key;
+    cout << lower_bound(v, key) << "\n";
+    return 0;
+}
